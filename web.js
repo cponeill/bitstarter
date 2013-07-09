41db9c1c6887a188675a9cf3d3f06504e2abc90d
cponeill <caseyoneill78@hotmail.com>
2013-07-09 15:08:41 +0000

    web.js test

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var fs - require('fs');
+  var buffer = new Buffer();
+  response.send(fs.readSyncFile('index.html').toString());
 });
 
 var port = process.env.PORT || 5000;
